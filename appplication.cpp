c13c7775eff9e9c87189e36e8f9799be228d89a3
unknown <zhilibenjamin.goh@digipen.edu>
2020-05-24 05:17:16 +0800

    Added my own array template class for usage ltr on in the project thanks!

--- appplication.cpp
+++ appplication.cpp
@@ -1,5 +1,16 @@
 #include <iostream>>
 
+template<typename T, size_t N>
+class myArray {
+	
+	T _internal[N];
+public:
+	size_t size() const {
+		return N;
+	}
+	
+};
+
 int main()
 {
 	std::cout << "Test Github" << std::endl;
